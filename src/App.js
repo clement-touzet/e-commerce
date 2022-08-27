382cdf690576e7beb5423c65ac09310347a83bcd
Clément Touzet <clement.touzet40@gmail.com>
2022-08-27 12:23:47 +0200

    Commerce products

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { commerce } from "./lib/commerce";
 
 import { Products, Navbar } from "./components";
+import { isValidInputTimeValue } from "@testing-library/user-event/dist/utils";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+
   return (
     <div>
       <Navbar />
-      <Products />
+      <Products products={products} />
     </div>
   );
 };
